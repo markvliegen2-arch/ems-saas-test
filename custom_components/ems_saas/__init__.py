a92cc818d652bef82e655da1d335d443521ed3f0
markvliegen2-arch <markvliegen2@gmail.com>
2026-07-27 18:42:18 +0200

    Update __init__.py

--- custom_components/ems_saas/__init__.py
+++ custom_components/ems_saas/__init__.py
@@ -1,1 +1,14 @@
-__init__.py
+"""EMS SaaS Universele Client Custom Integration."""
+import logging
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+_LOGGER = logging.getLogger(__name__)
+DOMAIN = "ems_saas"
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up EMS SaaS via de wizard UI."""
+    _LOGGER.info("EMS SaaS Integratie succesvol opgestart.")
+    
+    # Hier dwingen we straks het universele script mee te installeren bij de klant
+    return True
