ec201a9d0a29ee1b09481897af6231a6c9f87f45
markvliegen2-arch <markvliegen2@gmail.com>
2026-07-28 21:21:23 +0200

    Update config_flow.py

--- custom_components/ems_saas/config_flow.py
+++ custom_components/ems_saas/config_flow.py
@@ -5,15 +5,17 @@ import voluptuous as vol
 DOMAIN = "ems_saas"
 
 class EmsSaasConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Beheer de configuratieflow."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Eerste stap wanneer de klant de integratie inklikt."""
         if user_input is not None:
-            return self.async_create_entry(title="EMS SaaS Cloud", data=user_input)
+            return self.async_create_entry(title=f"EMS {user_input['inverter_brand']}", data=user_input)
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({})
+            data_schema=vol.Schema({
+                vol.Required("api_key"): str,
+                vol.Required("cloud_endpoint", default="https://mijn-ems-backend.onrender.com"): str,
+                vol.Required("inverter_brand", default="GoodWe"): vol.In(["GoodWe", "SolaX Power", "Huawei SUN2000", "Victron Energy"]),
+            })
         )
